c6cf8c517d62c80d79d6b64f2ae5a4defd5ea50a
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-12-26 17:00:56 +0530

    Section 16.6 : synchronous action creators.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { increment, decrement, add, subtract, storeResult, deleteResult } from '../../store/actions/actions';
+import * as actionTypes from '../../store/actions/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
     state = {
@@ -56,12 +57,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
-        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-        onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 10 }),
-        onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 15 }),
-        onStoreRersult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
-        onDeleteRersult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id })
+        onIncrementCounter: () => dispatch(increment()),
+        onDecrementCounter: () => dispatch(decrement()),
+        onAddCounter: () => dispatch(add(10)),
+        onSubtractCounter: () => dispatch(subtract(15)),
+        onStoreRersult: (result) => dispatch(storeResult(result)),
+        onDeleteRersult: (id) => dispatch(deleteResult(id))
     }
 }
 
