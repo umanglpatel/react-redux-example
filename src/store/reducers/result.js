651099963740402285ce3e580dd79ad912644e96
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-12-28 11:11:27 +0530

    Section 16.11 refactoring reducers utility functions.

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -8,19 +9,13 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
             // change data
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), val: action.result * 2 })
-            }
+            return updateObject(state, { results: state.results.concat({ id: new Date(), val: action.result * 2 }) });
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // newArray = [...state.results];
             // newArray.splice(id, 1); 
             const updatedArray = state.results.filter((result) => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject(state, { results: updatedArray });
     }
     return state;
 }
